7b3b9d8ac014bcf1b14bf5e79d813976f57f57bc
Yves Bertot <Yves.Bertot@inria.fr>
2023-04-06 14:33:17 +0200

    this shell script makes it more systematic to produce a large collection of tests (9 in the first version).

---
+++ theories/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in 0 1 2
+do
+  for j in 0 1 2
+  do
+    echo "Require Import String smooth_trajectories. Compute example_by_index $i $j true." \
+     | coqtop -R . trajectories | ../coq_output_to_postscript.pl > "output_"$i"_"$j"_true".ps
+  ps2pdf "output_"$i"_"$j"_true".ps
+  rm "output_"$i"_"$j"_true".ps
+  done
+done
+
