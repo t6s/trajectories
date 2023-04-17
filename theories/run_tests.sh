c93336e8a3c7767d52ec42d3e6c0ab12b2195faf
Yves Bertot <Yves.Bertot@inria.fr>
2023-04-17 13:47:17 +0200

    make run_tests.sh keep postscript files on demand

--- theories/run_tests.sh
+++ theories/run_tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$1" = "-keep-postscript" ]
+then
+keep_postscript=yes
+else
+keep_postscript=no
+fi
+
 for i in 0 1 2
 do
   for j in 0 1 2
@@ -7,7 +14,10 @@ do
     echo "Require Import String smooth_trajectories. Compute example_by_index $i $j true." \
      | coqtop -R . trajectories | ../coq_output_to_postscript.pl > "output_"$i"_"$j"_true".ps
   ps2pdf "output_"$i"_"$j"_true".ps
-  rm "output_"$i"_"$j"_true".ps
+  if [ "$keep_postscript" = yes ]
+  then true
+  else rm "output_"$i"_"$j"_true".ps
+  fi
   done
 done
 
