66587a7fe32f32a61373bbbede0b6324f6b44d52
Yves Bertot <Yves.Bertot@inria.fr>
2023-07-13 09:32:20 +0200

    ml extracted code for experiments in a web interface

---
+++ theories/extraction_command.v
@@ -0,0 +1,5 @@
+From trajectories Require Import smooth_trajectories.
+Require Import QArith.
+
+Extraction "smooth_trajectories" smooth_point_to_point example_bottom example_top
+  example_edge_sets example_point_spread_sets Qred.
